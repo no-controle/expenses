e4d9c7a13cd18d7e33c4be316f3b7683f5bcbd1d
jessyanjos <jessicarocha.anjos@gmail.com>
2019-10-15 11:53:23 -0300

    Using midje for tests

--- test/expenses/service_test.clj
+++ test/expenses/service_test.clj
@@ -1,5 +1,5 @@
 (ns expenses.service-test
-  (:require [clojure.test :refer :all]
+  (:require [midje.sweet :refer :all]
             [io.pedestal.test :refer :all]
             [io.pedestal.http :as bootstrap]
             [expenses.service :as service]))
@@ -7,12 +7,13 @@
 (def service
   (::bootstrap/service-fn (bootstrap/create-servlet service/service)))
 
-(deftest home-page-test
-  (is (=
-       (:body (response-for service :get "/"))
-       "Hello World!"))
-  (is (=
-       (:headers (response-for service :get "/"))
+(facts "true" 
+  (fact "true" 
+    (= 1 1) => true))
+
+(fact "home page"
+  (:body (response-for service :get "/")) => "Hello World!"
+  (:headers (response-for service :get "/")) => 
        {"Content-Type" "text/html;charset=UTF-8"
         "Strict-Transport-Security" "max-age=31536000; includeSubdomains"
         "X-Frame-Options" "DENY"
@@ -20,20 +21,16 @@
         "X-XSS-Protection" "1; mode=block"
         "X-Download-Options" "noopen"
         "X-Permitted-Cross-Domain-Policies" "none"
-        "Content-Security-Policy" "object-src 'none'; script-src 'unsafe-inline' 'unsafe-eval' 'strict-dynamic' https: http:;"})))
+        "Content-Security-Policy" "object-src 'none'; script-src 'unsafe-inline' 'unsafe-eval' 'strict-dynamic' https: http:;"})
 
-(deftest about-page-test
-  (is (.contains
-       (:body (response-for service :get "/about"))
-       "Clojure 1.9"))
-  (is (=
-       (:headers (response-for service :get "/about"))
-       {"Content-Type" "text/html;charset=UTF-8"
-        "Strict-Transport-Security" "max-age=31536000; includeSubdomains"
-        "X-Frame-Options" "DENY"
-        "X-Content-Type-Options" "nosniff"
-        "X-XSS-Protection" "1; mode=block"
-        "X-Download-Options" "noopen"
-        "X-Permitted-Cross-Domain-Policies" "none"
-        "Content-Security-Policy" "object-src 'none'; script-src 'unsafe-inline' 'unsafe-eval' 'strict-dynamic' https: http:;"})))
-
+(fact "about-page-test"
+  (:body (response-for service :get "/about")) => (contains "Clojure 1.10.1")
+  (:headers (response-for service :get "/about")) => 
+    {"Content-Type" "text/html;charset=UTF-8"
+     "Strict-Transport-Security" "max-age=31536000; includeSubdomains"
+     "X-Frame-Options" "DENY"
+     "X-Content-Type-Options" "nosniff"
+     "X-XSS-Protection" "1; mode=block"
+     "X-Download-Options" "noopen"
+     "X-Permitted-Cross-Domain-Policies" "none"
+     "Content-Security-Policy" "object-src 'none'; script-src 'unsafe-inline' 'unsafe-eval' 'strict-dynamic' https: http:;"})
